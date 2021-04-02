8089544d0de9d487a12b38c190bf6a51759fc4cb
jermz678 <jermz678@live.com>
2021-04-02 18:16:52 -0500

    put text area in HTML and created function to store user input

--- style/scipt.js
+++ style/scipt.js
@@ -1,33 +1,55 @@
-//GIVEN I am using a daily planner to create a schedule
-//WHEN I scroll down
 //WHEN I view the time blocks for that day
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
-//WHEN I click into a time block
-//THEN I can enter an event
-//WHEN I click the save button for that time block
-//THEN the text for that event is saved in local storage
-//WHEN I refresh the page
-//THEN the saved events persist
+
 
 var currentDay = document.querySelector("#currentDay")
-//var userInput = document.querySelector(".textarea")
 
-//getting current day
-dayjs().format("YYYY-MMMM-DD");
-console.log(dayjs());
 //displaying current day on header
 currentDay.innerHTML = dayjs().format("MMMM-DD-YYYY");
 
+
+//variables for each text area
+var task1 = document.getElementById("task1");
+var task2 = document.getElementById("task2");
+var task3 = document.getElementById("task3");
+var task4 = document.getElementById("task4");
+var task5 = document.getElementById("task5");
+var task6 = document.getElementById("task6");
+var task7 = document.getElementById("task7");
+var task8 = document.getElementById("task8");
+var task9 = document.getElementById("task9");
+
+//take what user puts in and save to local storage
+var saveUserInput = function(){
+    localStorage.setItem("task1", task1.value)
+    localStorage.setItem("task2", task2.value)
+    localStorage.setItem("task3", task3.value)
+    localStorage.setItem("task4", task4.value)
+    localStorage.setItem("task5", task5.value)
+    localStorage.setItem("task6", task6.value)
+    localStorage.setItem("task7", task7.value)
+    localStorage.setItem("task8", task8.value)
+    localStorage.setItem("task9", task9.value)
+}
+    task1.innerHTML = localStorage.getItem("task1")
+    task2.innerHTML = localStorage.getItem("task2")
+    task3.innerHTML = localStorage.getItem("task3")
+    task4.innerHTML = localStorage.getItem("task4")
+    task5.innerHTML = localStorage.getItem("task5")
+    task6.innerHTML = localStorage.getItem("task6")
+    task7.innerHTML = localStorage.getItem("task7")
+    task8.innerHTML = localStorage.getItem("task8")
+    task9.innerHTML = localStorage.getItem("task9")
+
+//variable to hold current time
+var currentHour = (dayjs().hour());
+console.log(currentHour)
+
 //making time blocks change based on past,present,future
 //using if and else statement
-//put in a function 
-        
-$(".time-block").on("click", ".description", function(){
-    console.log(this)
-    var input = $(this).text()
-    .trim()
-    var userInput = $("<textarea>")
-    .addClass("textarea col-8")
-    $(this).replaceWith(userInput);
-    userInput.trigger("focus");
-})
+//put in a function
+var checkTheHour = function(){
+    if (dayjs().hour() < parseInt($(this).attr("id"))){
+        $("#description").addClass("present")
+    }
+}
