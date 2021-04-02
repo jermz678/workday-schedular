3560eb317e58f44297491b1305ef3294bb3c468f
jermz678 <jermz678@live.com>
2021-04-02 14:54:30 -0500

    clicking on time-block allows user to put in text

--- style/scipt.js
+++ style/scipt.js
@@ -1,8 +1,5 @@
 //GIVEN I am using a daily planner to create a schedule
-//WHEN I open the planner
-//THEN the current day is displayed at the top of the calendar
 //WHEN I scroll down
-//THEN I am presented with time blocks for standard business hours
 //WHEN I view the time blocks for that day
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
 //WHEN I click into a time block
@@ -11,10 +8,26 @@
 //THEN the text for that event is saved in local storage
 //WHEN I refresh the page
 //THEN the saved events persist
+
 var currentDay = document.querySelector("#currentDay")
+//var userInput = document.querySelector(".textarea")
 
 //getting current day
 dayjs().format("YYYY-MMMM-DD");
 console.log(dayjs());
 //displaying current day on header
 currentDay.innerHTML = dayjs().format("MMMM-DD-YYYY");
+
+//making time blocks change based on past,present,future
+//using if and else statement
+//put in a function 
+        
+$(".time-block").on("click", ".description", function(){
+    console.log(this)
+    var input = $(this).text()
+    .trim()
+    var userInput = $("<textarea>")
+    .addClass("textarea col-8")
+    $(this).replaceWith(userInput);
+    userInput.trigger("focus");
+})
