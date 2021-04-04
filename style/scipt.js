139d3cb67550051ff6a356b5632bc2f142e65ce5
jermz678 <jermz678@live.com>
2021-04-04 15:06:35 -0500

    added a function with if, if else, and else statements to check the hour to current time.  Added a set interval so the app will update every minute

--- style/scipt.js
+++ style/scipt.js
@@ -2,12 +2,10 @@
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
 
 
-var currentDay = document.querySelector("#currentDay")
+
 
 //displaying current day on header
-currentDay.innerHTML = dayjs().format("MMMM-DD-YYYY");
-
-
+$("#currentDay").html(dayjs().format("MMMM-DD-YYYY")) 
 
 //display what is in local storage
 var showSavedTasks = function(){
@@ -22,6 +20,7 @@ var showSavedTasks = function(){
     $("#task9").html(localStorage.getItem("task9")) 
 }
 showSavedTasks();
+
 //take what user puts in and save to local storage
 var saveUserInput = function(){
     localStorage.setItem("task1", $.trim($("#task1").val()))
@@ -35,16 +34,118 @@ var saveUserInput = function(){
     localStorage.setItem("task9", $.trim($("#task9").val()))
 }
 
-
 //variable to hold current time
-var currentHour = (dayjs().hour());
+var currentHour = dayjs().hour();
 console.log(currentHour)
-console.log($.trim($("#task1").val()))
+
+//variables to get id value 
+var hour9 = document.getElementById("09").id;
+var hour10 = document.getElementById("10").id;
+var hour11 = document.getElementById("11").id;
+var hour12 = document.getElementById("12").id;
+var hour13 = document.getElementById("13").id;
+var hour14 = document.getElementById("14").id;
+var hour15 = document.getElementById("15").id;
+var hour16 = document.getElementById("16").id;
+var hour17 = document.getElementById("17").id;
+
 //making time blocks change based on past,present,future
 //using if and else statement
 //put in a function
+
 var checkTheHour = function(){
-    if( currentHour.toString() === "19"){
-    console.log("it worked")}
     
+    if( currentHour === parseInt(hour9)){
+        $("#task1").addClass("present");
+    }   
+    else if( currentHour < parseInt(hour9) ){
+        $("#task1").addClass("future");
+    }
+    else {
+        $("#task1").addClass("past");
+    }
+    //checking 10 am
+    if( currentHour === parseInt(hour10)){
+        $("#task2").addClass("present");
+    }   
+    else if( currentHour < parseInt(hour10) ){
+        $("#task2").addClass("future");
+    }
+    else {
+        $("#task2").addClass("past");
+    }
+    //checking 11 am
+    if( currentHour === parseInt(hour11)){
+        $("#task3").addClass("present");
+    }   
+    else if( currentHour < parseInt(hour11) ){
+        $("#task3").addClass("future");
+    }
+    else {
+        $("#task3").addClass("past");
+    }
+    //checking 12 pm
+    if( currentHour === parseInt(hour12)){
+         $("#task4").addClass("present");
+    }   
+    else if( currentHour < parseInt(hour12) ){
+        $("#task4").addClass("future");
+    }
+    else {
+        $("#task4").addClass("past");
+    }
+    //checking 1 pm
+    if( currentHour === parseInt(hour13)){
+        $("#task1").addClass("present");
+    }   
+    else if( currentHour < parseInt(hour13) ){
+        $("#task5").addClass("future");
+    }
+    else {
+         $("#task5").addClass("past");
+    }
+    //checking 2 pm 
+    if( currentHour === parseInt(hour14)){
+        $("#task6").addClass("present");
+    }   
+    else if( currentHour < parseInt(hour14) ){
+        $("#task6").addClass("future");
+    }
+    else {
+        $("#task6").addClass("past");
+    }
+    //checking 3 pm
+    if( currentHour === parseInt(hour15)){
+        $("#task7").addClass("present");
+    }   
+    else if( currentHour < parseInt(hour15) ){
+        $("#task7").addClass("future");
+    }
+    else {
+        $("#task7").addClass("past");
+    }
+    //checking 4 pm
+    if( currentHour === parseInt(hour16)){
+        $("#task8").addClass("present");
+    }   
+    else if( currentHour < parseInt(hour16) ){
+        $("#task8").addClass("future");
+    }
+    else {
+        $("#task8").addClass("past");
+    }
+    //checking 5 pm
+    if( currentHour === parseInt(hour17)){
+        $("#task9").addClass("present");
+    }   
+    else if( currentHour < parseInt(hour17)){
+        $("#task9").addClass("future");
+    }
+    else {
+    $("#task9").addClass("past");
+    }
 }
+checkTheHour()
+setInterval(function(){
+    checkTheHour();
+}, 60 * 1000);
