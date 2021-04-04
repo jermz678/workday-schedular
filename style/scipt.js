b7282502871d86246b0e8853085f73a6cdc208d6
jermz678 <jermz678@live.com>
2021-04-04 12:13:29 -0500

    used jqery to create a function that shows saved tasks from local storage

--- style/scipt.js
+++ style/scipt.js
@@ -8,17 +8,20 @@ var currentDay = document.querySelector("#currentDay")
 currentDay.innerHTML = dayjs().format("MMMM-DD-YYYY");
 
 
-//variables for each text area
-//var task1 = document.getElementById("task1");
-//var task2 = document.getElementById("task2");
-//var task3 = document.getElementById("task3");
-//var task4 = document.getElementById("task4");
-//var task5 = document.getElementById("task5");
-//var task6 = document.getElementById("task6");
-//var task7 = document.getElementById("task7");
-//var task8 = document.getElementById("task8");
-//var task9 = document.getElementById("task9");
 
+//display what is in local storage
+var showSavedTasks = function(){
+    $("#task1").html(localStorage.getItem("task1")) 
+    $("#task2").html(localStorage.getItem("task2")) 
+    $("#task3").html(localStorage.getItem("task3")) 
+    $("#task4").html(localStorage.getItem("task4")) 
+    $("#task5").html(localStorage.getItem("task5")) 
+    $("#task6").html(localStorage.getItem("task6")) 
+    $("#task7").html(localStorage.getItem("task7")) 
+    $("#task8").html(localStorage.getItem("task8")) 
+    $("#task9").html(localStorage.getItem("task9")) 
+}
+showSavedTasks();
 //take what user puts in and save to local storage
 var saveUserInput = function(){
     localStorage.setItem("task1", $.trim($("#task1").val()))
@@ -31,15 +34,7 @@ var saveUserInput = function(){
     localStorage.setItem("task8", $.trim($("#task8").val()))
     localStorage.setItem("task9", $.trim($("#task9").val()))
 }
-    task1.innerHTML = localStorage.getItem("task1")
-    task2.innerHTML = localStorage.getItem("task2")
-    task3.innerHTML = localStorage.getItem("task3")
-    task4.innerHTML = localStorage.getItem("task4")
-    task5.innerHTML = localStorage.getItem("task5")
-    task6.innerHTML = localStorage.getItem("task6")
-    task7.innerHTML = localStorage.getItem("task7")
-    task8.innerHTML = localStorage.getItem("task8")
-    task9.innerHTML = localStorage.getItem("task9")
+
 
 //variable to hold current time
 var currentHour = (dayjs().hour());
